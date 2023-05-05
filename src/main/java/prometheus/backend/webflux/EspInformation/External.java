cb7e7ce5197c6d052684bae1034fe27bc45f59b8
Narawich Saphimarn <narawichsaphimarn@gmail.com>
2023-05-05 23:39:20 +0700

    Update

--- src/main/java/prometheus/backend/webflux/EspInformation/External.java
+++ src/main/java/prometheus/backend/webflux/EspInformation/External.java
@@ -1,6 +1,7 @@
 package prometheus.backend.webflux.EspInformation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import prometheus.backend.webflux.EspInformation.entity.Position;
 import prometheus.backend.webflux.EspInformation.service.PositionService;
@@ -21,15 +22,26 @@ public class External {
     }
 
     @GetMapping
+    @CrossOrigin("*")
+    @ResponseStatus(HttpStatus.OK)
     public Flux<Position> getIp(@RequestParam("ip") String ip) {
         return this.positionService.getByIp(ip);
     }
 
     @PostMapping
+    @CrossOrigin("*")
+    @ResponseStatus(HttpStatus.OK)
     public Flux<Position> create(@RequestBody ArrayList<Position> position) {
         return Flux.fromIterable(position).flatMapSequential(position1 -> {
             return this.positionService.findByName(position1.getName()).switchIfEmpty(this.positionService.createPosition(position1))
-                    .flatMap(this.positionService::createPosition)
+                    .flatMap(position2 -> {
+                        position2.setCurrent(position1.getCurrent());
+                        position2.setMode(position1.getMode());
+                        position2.setTo_position(position1.getTo_position());
+                        position2.setIp_address(position2.getIp_address());
+                        position2.setPort(position2.getPort());
+                        return this.positionService.createPosition(position2);
+                    })
                     .doOnError(throwable -> log.error(throwable.getMessage()));
         }).subscribeOn(parallel()).doOnError(throwable -> log.error(throwable.getMessage()));
     }
